4db0ee48d05ff0f591d1253c558aee7a23dcbe21
sanjeet choudhary <sanjeet.chaudhary@outlook.com>
2018-04-10 02:56:16 +0530

    added new S-ERP Project with server configuration

---
+++ GlobalConfigServer/src/main/java/org/serp/server/GlobalConfigServerApplication.java
@@ -0,0 +1,20 @@
+package org.serp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableWebSecurity
+
+public class GlobalConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalConfigServerApplication.class, args);
+	}
+}
